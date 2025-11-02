6a3161fa5a17ea6b4172f46111fde73eabc99ee2
Franco_SJ <jean.jara.fs@gmail.com>
2025-11-02 20:08:03 -0300

    feat: crear modelos iniciales Usuario, Habitacion, Cliente, Historial, Reserva

--- reservas_hotel/models.py
+++ reservas_hotel/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=15)
+    password = models.CharField(max_length=20)
+
+class Habitacion(models.Model):
+    tipo = models.CharField(max_length=100)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Reserva(models.Model):
+    habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha_reserva = models.DateTimeField()
+    monto = models.IntegerField()
+
+class Historial(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=200)
+    tabla_afectada = models.CharField(max_length=100)
+    fecha = models.DateTimeField()
